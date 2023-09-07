0ef7f946394aa21ac8dc6994d2e093872b819dff
collin4210 <collinsasuke@gmail.com>
2023-09-07 21:21:19 +0200

    baked the cake!

--- main.js
+++ main.js
@@ -1,84 +1,93 @@
-main.js
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017";
+var url = "mongodb://localhost:27017/FDDW";
 const express = require('express');
 const PORT = process.env.port || 5555;
 const api = express();
 
+
 api.listen(PORT, () => {
-    console.log("API läuft!", PORT)
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("FDDW");
+    dbo.createCollection("user", function (err, res) {
+      if (err) throw err;
+      console.log("Collection created!");
+    });
+    db.close();
+  });
+  console.log("API läuft!", PORT)
 });
 
-api.get('/',(req,res) => {
-        res.send("Et klappt");
+api.get('/Cake', (req, res) => {
+
+  res.send("The cake is a Lie! sincerly aperture science");
+
 });
 
 //alle User ausgeben
-api.get('/user', (req,res) => {
+api.get('/user', (req, res) => {
 
-        console.log("Freddy Fazbear!");
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("FDDW");
+    dbo.collection("user").find({}, function (err, result) {
+      if (err) throw err;
+      console.log(result);
+      db.close();
+    });
+  });
 
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db("FDDW");
-        dbo.collection("user").find({}, function(err, result) {
-          if (err) throw err;
-          console.log(result);
-          db.close();
-        });
-      });
-
-      res.status(200).send(result)
+  res.status(200).send(result)
 
 });
 
 
 //einen User ausgeben
-api.get('/user/:uName', (req,res) => {
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db("FDDW");
-        dbo.collection("user").find({username : req.params.uName}, function(err, result) {
-          if (err) throw err;
-          console.log(result);
-          db.close();
-        });
-      });
-      res.status(200).send(result)
+api.get('/user/:uName', (req, res) => {
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("FDDW");
+    dbo.collection("user").find({ username: req.params.uName }, function (err, result) {
+      if (err) throw err;
+      console.log(result);
+      db.close();
+    });
+  });
+  res.status(200).send(result)
 });
 
 //einen User löschen
-api.delete('/user/:uName', (req,res) => {
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db("FDDW");
-        dbo.collection("user").deleteOne({username : req.params.uName}, function(err, obj) {
-          if (err) throw err;
-          console.log("One entry deleted");
-          db.close();
-        });
-      });
-      res.status(200).send("User deleted")
+api.delete('/user/:uName', (req, res) => {
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("FDDW");
+    dbo.collection("user").deleteOne({ username: req.params.uName }, function (err, obj) {
+      if (err) throw err;
+      console.log("One entry deleted");
+      db.close();
+    });
+  });
+  res.status(200).send("User deleted")
 
 });
 
 //einen User hinzufügen
- api.post('/user', (req,res) => {
-  MongoClient.connect(url, function(err, db) {
+api.post('/user', (req, res) => {
+  MongoClient.connect(url, function (err, db) {
     if (err) throw err;
     var dbo = db.db("FDDW");
     var myobj = {
       username: req.body.username,
       vorname: req.body.vorname,
-      nachname: req.body.nachname, 
+      nachname: req.body.nachname,
       plz: req.body.plz,
       stadt: req.body.stadt,
       strasse: req.body.strasse,
       hnr: req.body.hnr,
       werkzeug: []
-      
+
     };
-    dbo.collection("user").insertOne(myobj, function(err, res) {
+    dbo.collection("user").insertOne(myobj, function (err, res) {
       if (err) throw err;
       console.log("1 user created");
       db.close();
@@ -88,24 +97,28 @@ api.delete('/user/:uName', (req,res) => {
 });
 
 //einen User bearbeiten
-api.put('/user/:uID', (req,res) => {
-    // TODO
+api.put('/user/:uID', (req, res) => {
+  // TODO
 
 });
 
 
 //ein Werkzeug hinzufügen
-api.post('/user/:uName/tool', (req,res) => {
-  MongoClient.connect(url, function(err, db) {
+api.post('/user/:uName/tool', (req, res) => {
+  MongoClient.connect(url, function (err, db) {
     if (err) throw err;
     var dbo = db.db("FDDW");
     var myquery = { username: req.params.uName };
-    var newvalues = { $push: {werkzeug : {
-      bezeichnung: req.body.bezeichnung,
-      beschreibung: req.body.beschreibung,
-      ausgeliehen: false
-    }} };
-    dbo.collection("customers").updateOne(myquery, newvalues, function(err, res) {
+    var newvalues = {
+      $push: {
+        werkzeug: {
+          bezeichnung: req.body.bezeichnung,
+          beschreibung: req.body.beschreibung,
+          ausgeliehen: false
+        }
+      }
+    };
+    dbo.collection("customers").updateOne(myquery, newvalues, function (err, res) {
       if (err) throw err;
       console.log("1 document updated");
       db.close();
@@ -116,17 +129,17 @@ api.post('/user/:uName/tool', (req,res) => {
 });
 
 //alle Werkzeuge ausgeben
-api.get('/tool', (req,res) => {
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db("FDDW");
-        dbo.collection("user").find({},{username: 0, vorname: 0, nachname: 0, plz:0, stadt: 0, strasse: 0,hnr: 0, werkzeug: 1}, function(err, result) {
-          if (err) throw err;
-          console.log(result);
-          db.close();
-        });
-      });
-      res.status(200).send(result)
+api.get('/tool', (req, res) => {
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("FDDW");
+    dbo.collection("user").find({}, { username: 0, vorname: 0, nachname: 0, plz: 0, stadt: 0, strasse: 0, hnr: 0, werkzeug: 1 }, function (err, result) {
+      if (err) throw err;
+      console.log(result);
+      db.close();
+    });
+  });
+  res.status(200).send(result)
 
 });
 
