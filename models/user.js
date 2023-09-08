1358cf617951dded510e953e496c3b8fe22a7ea4
collin4210 <collinsasuke@gmail.com>
2023-09-08 12:47:29 +0200

    test

---
+++ models/user.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var UserSchema = new mongoose.Schema({
+ username: String,
+ name: String,
+ nachname: String,
+ PLZ: Number,
+ Adresse: String,
+ Email: String,
+ 
+ updated_at: { type: Date, default: Date.now },
+});
+module.exports = mongoose.model('User', UserSchema);
