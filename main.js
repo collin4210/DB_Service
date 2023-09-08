4dd868cd85b35f36df1c89c7a2a3afa3146b27c7
collin4210 <collinsasuke@gmail.com>
2023-09-08 12:41:39 +0200

    User Route added

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ var mongoose = require('mongoose');
 const express = require('express');
 const PORT = process.env.port || 5555;
 const api = express();
+var user = require('./routes/user');
+app.use('/user', user);
+
 
 mongoose.Promise = global.Promise;
 
